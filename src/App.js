a785c4dc13a500159164f87e621da8b9e44e3f50
ridi1993 <rkatundsi@tetra.al>
2023-01-05 18:06:01 +0100

    inital styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import "./App.css";
 
 const App = () => {
   return (
-    <h1 className='underline text-3xl'>App Dashboard</h1>
-  )
-}
+    <BrowserRouter>
+      <div className="flex relative  dark:bg-main-dark-bg">
+        <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+          <TooltipComponent content="Settings" position="Top">
+            <button type="button" className="text-4xl p-3 hover:drop-shadow-xl text-white" style={{background: "blue", borderRadius: "50%" }}>
+              <FiSettings />
+            </button>
+          </TooltipComponent>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
