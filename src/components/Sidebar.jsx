fbc51ba3003c10c7783359a2d84d1df40ac0c30e
ridi1993 <rkatundsi@tetra.al>
2023-01-12 09:52:36 +0100

    style sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,36 +1,47 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import{SiShopware} from 'react-icons/si'
-import {MdOutlineCancel} from 'react-icons/md'  
-import {TooltipComponent} from '@syncfusion/ej2-react-popups'
-import {links} from '../data/dummy'
-
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { SiShopware } from "react-icons/si";
+import { MdOutlineCancel } from "react-icons/md";
+import { links } from "../data/dummy";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 const Sidebar = () => {
-  const activeMenu = true
+  const activeMenu = true;
   return (
-    <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
-{activeMenu && (
-  <>
-  <div className="flex justify-between items-center">
-
-    <Link to="/" onClick={()=>{}} className='items-center gap-3 ml-3 mt-4 flex text-xl 
-    font-extrabold tracking-tight dark:text-white text-slate-900'>
-    <SiShopware/><span>Shoppy</span> 
-    </Link>
-    <TooltipComponent content="Menu" position='BottomCenter'>
-<button className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden'>
-
-<MdOutlineCancel/>
-</button>
-    </TooltipComponent>
-  </div>
-  </>
-
-
-) }
+    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+      {activeMenu && (
+        <>
+          <div className="flex justify-between items-center">
+            <Link
+              to="/"
+              onClick={() => {}}
+              className="items-center gap-3 ml-3 mt-4 flex text-xl 
+    font-extrabold tracking-tight dark:text-white text-slate-900"
+            >
+              <SiShopware />
+              <span>Shoppy</span>
+            </Link>
+            <TooltipComponent content="Menu" position="BottomCenter">
+              <button
+                type="button"
+                onClick={() => {}}
+                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+              >
+                <MdOutlineCancel />
+              </button>
+            </TooltipComponent>
+          </div>
+          <div className="mt-10">
+            {links.map((item) => (
+              <div key={item.title}>
+                <p className="text-gray-400 m-3 mt-4 uppercase">{item.title}</p>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
